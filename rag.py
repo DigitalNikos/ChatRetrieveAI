9e9fb0becfceb1a1b631ecc5056590f5b3fd9d4f
Nikos Lithoxopoulos <nikoslithoxopoulos@Nikoss-Mac-mini.local>
2024-05-15 12:12:22 +0200

    Add initialize function, agent, agent_executor, tools

--- rag.py
+++ rag.py
@@ -8,9 +8,14 @@ from langchain.schema.runnable import RunnablePassthrough
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores.utils import filter_complex_metadata
 from config import Config as cfg
+from langchain.tools.retriever import create_retriever_tool
 
 from langchain import hub
+from langchain.agents import create_react_agent
+from langchain.agents import AgentExecutor
+from typing import List
 
+from langchain_core.documents import Document
 
 class ChatPDF:
     vector_store = None
@@ -21,13 +26,17 @@ class ChatPDF:
         print("Constructor ChatPDF called...")
         self.model = ChatOllama(model=cfg.MODEL)
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=cfg.SPLITTER_CHUNK_SIZE, chunk_overlap=cfg.SPLITTER_CHUNK_OVERLAP)
+
         self.prompt =  hub.pull("hwchase17/react-chat")
 
-    def ingest(self, pdf_file_path: str):
-        docs = PyPDFLoader(file_path=pdf_file_path).load()
-        chunks = self.text_splitter.split_documents(docs)
-        chunks = filter_complex_metadata(chunks)
-
+        self.agent = None
+        self.agent_executor = None
+        self.tools = []
+        self.chat_history = ""
+        self.retriever = None
+    
+    def initialize(self, chunks: List[Document]):
+        print("Initializing")
         vector_store = Chroma.from_documents(documents=chunks, embedding=FastEmbedEmbeddings())
         self.retriever = vector_store.as_retriever(
             search_type="similarity_score_threshold",
@@ -37,18 +46,45 @@ class ChatPDF:
             },
         )
 
-        self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
-                      | self.prompt
-                      | self.model
-                      | StrOutputParser())
+        retriever_tool = create_retriever_tool(self.retriever, name="Private knowledge base", description="YOU MUST ALWAYS USE THIS AS YOUR FIRST TOOL. Lookup information in a private knowledge base.")
+        self.tools = [retriever_tool]
+
+        self.agent = create_react_agent(llm=self.model, tools=self.tools, prompt=self.prompt, )
+        self.agent_executor = AgentExecutor(agent=self.agent, tools=self.tools, verbose=True, max_iterations=4, return_intermediate_steps=True, handle_parsing_errors=True)
+
+    def ingest(self, pdf_file_path: str):
+        print("Ingesting...")
+        docs = PyPDFLoader(file_path=pdf_file_path).load()
+
+        chunks = self.text_splitter.split_documents(docs)
+        chunks = filter_complex_metadata(chunks)
+
+        if not self.retriever:
+            self.initialize(chunks)
+        else:
+            self.retriever.add_documents(chunks)
+
+        
+
+
+        # self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
+        #               | self.prompt
+        #               | self.model
+        #               | StrOutputParser())
 
     def ask(self, query: str):
-        if not self.chain:
+        if not self.agent_executor:
             return "Please, add a PDF document first."
-
-        return self.chain.invoke(query)
+        result = self.agent_executor.invoke({"input": query, "chat_history": self.chat_history})
+        answer = result["output"]
+        self.chat_history += "Human:" + query + "\n" + "Assistant (YOU)" + answer + "\n"
+        print(answer)
+        return answer
 
     def clear(self):
         self.vector_store = None
         self.retriever = None
         self.chain = None
+        self.agent = None
+        self.agent_executor = None
+        self.tools = []
