8dcb40bc2d8afa02283fc856fe13a9bf5509caa2
Nikos Lithoxopoulos <nikoslithoxopoulos@Nikoss-Mac-mini.local>
2024-07-24 20:29:41 +0200

    modify requirements.txt and vectorbd.py

--- rag/vectordb.py
+++ rag/vectordb.py
@@ -1,6 +1,7 @@
 
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import FastEmbedEmbeddings
+# from langchain_community.embeddings import FastEmbedEmbeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.vectorstores.utils import filter_complex_metadata
 from typing import List
 from langchain_core.documents import Document
@@ -11,7 +12,15 @@ class VectorDB:
         self.vector_store = None
 
     def initialize(self, chunks: List[Document]):
-        self.vector_store = Chroma.from_documents(documents=chunks, collection_name="rag-chroma", embedding=FastEmbedEmbeddings())
+        model_name = "BAAI/bge-large-en"
+        model_kwargs = {'device': 'cpu'}
+        encode_kwargs = {'normalize_embeddings': True}
+        hf = HuggingFaceBgeEmbeddings(
+            model_name=model_name,
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs
+        )
+        self.vector_store = Chroma.from_documents(documents=chunks, collection_name="rag-chroma", embedding=hf)
 
     def add_documents(self, chunks: List[Document]):
         if not self.vector_store:
