ba4f936527208a25311a281de5a3e274199a4b2d
Nikos Lithoxopoulos <nikoslithoxopoulos@Nikoss-Mac-mini.local>
2024-08-18 20:33:33 +0200

    Change utils

--- utils/extract_source.py
+++ utils/extract_source.py
@@ -1,13 +1,31 @@
 import tempfile
 import os
+import hashlib
 import requests
+from enum import Enum, auto
+
+class UploadStatus(Enum):
+    SUCCESS = auto()
+    INVALID_URL = auto()
+    ERROR = auto()
+    INVALID_DOMAIN = auto()
 
 def upload_pdf(domain: str, st):
     print("Calling =>extract_source.py - upload_pdf()")
     
     if st.session_state["file_uploader"]:
         file = st.session_state["file_uploader"][-1]
-        print(f"File to upload again Name: {file.name}")
+        
+        # Calculate the hash of the current file
+        file_hash = hashlib.md5(file.getbuffer()).hexdigest()
+        
+        # Check if the file has already been uploaded
+        for uploaded_file in st.session_state["file_uploader"][:-1]:
+            existing_file_hash = hashlib.md5(uploaded_file.getbuffer()).hexdigest()
+            if file_hash == existing_file_hash:
+                # st.warning(f"The document '{file.name}' has already been uploaded.")
+                return  {'status': UploadStatus.INVALID_URL, 'file_name': file.name}
+        
         _, file_extension = os.path.splitext(file.name)
         file_extension = file_extension.lower()
 
@@ -18,24 +36,35 @@ def upload_pdf(domain: str, st):
         with st.session_state["ingestion_spinner"], st.spinner(f"Ingesting {file.name}"):
             file_datails = {'file_path': file_path, 'source_extension': file_extension, 'file_name': file.name, 'domain': domain,}
             answer = st.session_state["assistant"].ingest(file_datails)
+            
+            print('Upload txt:', answer)
+            
             if answer == "no":
-                st.session_state["messages"].append(("Document does not fall within the specified domain", False))
-            # elif answer == "already_ingested":
-            #     st.session_state["messages"].append((f"The document '{file.name}' has already been ingested.", False))
+                return {'status': UploadStatus.INVALID_DOMAIN, 'file_name': file.name}
+            
+        st.session_state['file_names'].append(file.name)
         os.remove(file_path)
+        return  {'status': UploadStatus.SUCCESS, 'file_name': file.name}
+
 
 def upload_url(domain: str, st):
     print("Calling =>extract_source.py - upload_url()")
-    file_datails = {'url': st.session_state["url_upload"], 'source_extension': "url", 'domain': domain,'file_name': st.session_state["url_upload"]}
+    file_datails = {
+        'url': st.session_state["url_upload"], 
+        'source_extension': "url", 
+        'domain': domain,
+        'file_name': st.session_state["url_upload"]
+    }
+    
     try:
         answer = st.session_state["assistant"].ingest(file_datails)
+        print('AnswerURL:', answer)
         if answer == "no":
-            st.session_state["messages"].append(("Document does not fall within the specified domain", False))
-        # elif answer == "already_ingested":
-        #         st.session_state["messages"].append((f"The URL '{st.session_state['url_upload']}' has already been ingested.", False))
+            return {'status': UploadStatus.INVALID_DOMAIN}
     except requests.exceptions.MissingSchema:
-        st.error("Invalid URL: No scheme supplied. Please include 'http://' or 'https://'.")
+        return {'status': UploadStatus.INVALID_URL}
     except requests.exceptions.RequestException as e:
         st.error(f"An error occurred while processing the URL: {str(e)}")
+        return {'status': UploadStatus.ERROR}
     
-    
+    return {'status': UploadStatus.SUCCESS, 'url': st.session_state["url_upload"]}
--- utils/text_doc_processing.py
+++ utils/text_doc_processing.py
@@ -1,26 +1,20 @@
 from langchain_core.documents import Document
 from langchain_core.messages import HumanMessage, AIMessage
 import re
+from urllib.parse import urlparse
 
-def clean_text(chunks, file_name:str):
+def clean_text(chunks, file_name: str):
     print("Calling =>clean_text.py - clean_text()")
-    # Remove HTML tags
-    # text = BeautifulSoup(text, "lxml").get_text()
     
     for chunk in chunks:
+        # Remove newline characters
+        chunk.page_content = chunk.page_content.replace('\n', ' ')
         
-        # Remove unwanted characters but keep alphanumeric, spaces, commas, and dots
-        chunk.page_content = re.sub(r'[^A-Za-z0-9\s,.]', '', chunk.page_content)
-        
-        # Replace multiple spaces with a single space
+        # Replace multiple spaces with a single space 
         chunk.page_content = re.sub(r'\s+', ' ', chunk.page_content).strip()
         
-        # Convert text to lowercase
-        chunk.page_content = chunk.page_content.lower()
-    
         chunk.metadata['source'] = file_name
-    
-    # print("chunks in clear_text: ", chunks)
+
     
     return chunks
 
@@ -32,14 +26,17 @@ def format_rag_output(rag_output):
     if isinstance(metadata, dict):
         if 'sources' in metadata:
             sources.update(metadata['sources'])
+            
         if 'source' in metadata:
             source = metadata['source']
             page = metadata.get('page')
             if page:
                 source = f"{source}, page {page}"
             sources.add(source)
+            
         if 'link' in metadata:
             sources.update(metadata['link'])
+            
     elif isinstance(metadata, list):
         for item in metadata:
             sources.add(item['link'])
@@ -56,32 +53,27 @@ def convert_str_to_document(input:str):
         # Split into individual items
         items = re.split(r'\], \[', cleaned_string)
 
-        # Initialize list to hold the parsed dictionaries
+        
         documents = []
 
-        # Parse each item
         for item in items:
-            # Initialize dictionary
             parsed_dict = {}
             
             # Find all key-value pairs
             key_value_pairs = re.findall(r'(\w+):\s([^,]+)', item)
             
-            # Fill the dictionary
             for key, value in key_value_pairs:
                 parsed_dict[key] = value.strip()
             
-            # Create Document instance
             doc = Document(
                 page_content=parsed_dict['snippet'],
                 metadata={
-                    # 'source': parsed_dict['title'],
                     'link': parsed_dict['link']
                 }
             )
             
-            # Append to the list
             documents.append(doc)
+            
         return documents
 
 def normalize_documents(documents):
@@ -109,26 +101,28 @@ def normalize_documents(documents):
     
     return normalized_documents
 
+        
 def format_final_answer(answer):
-        if 'answer' in answer and isinstance(answer['answer'], dict):
-            response = answer['answer'].get('answer', 'No answer provided.')
-            metadata = answer['answer'].get('metadata', 'No metadata available.')
-            return f"{response}\n\nMetadata: {metadata}"
-        else:
-            return answer.get('answer', 'No answer provided.')
+    if 'answer' in answer and isinstance(answer['answer'], dict):
+        response = answer['answer'].get('answer', 'No answer provided.')
+        metadata = answer['answer'].get('metadata', 'No metadata available.')
+        source = "No metadata available"
+        # Check if metadata is a string
+        if isinstance(metadata, str):
+            # Attempt to parse the source and page if possible
+            match = re.search(r"source='([^']+)'(?: - page: (\d+))?", metadata)
+            if match:
+                source = match.group(1)
+                
+        
+        return f"{response}\n\nMetadata: {source}"
+    else:
+        return answer.get('answer', 'No answer provided.')
         
 
 
 def extract_limited_chat_history(chat_rephrased_history, max_length=3500):
-    """
-    Extract chat history messages, ensuring the total character count doesn't exceed max_length.
 
-    Args:
-        max_length (int): Maximum total character count for the messages.
-
-    Returns:
-        list: A list of messages (HumanMessage and AIMessage), within the character limit.
-    """
     current_length = 0
     chat_messages = []
 
@@ -143,3 +137,11 @@ def extract_limited_chat_history(chat_rephrased_history, max_length=3500):
             current_length += message_length
     
     return chat_messages
+
+
+def trim_url_to_domain(original_url):
+
+    parsed_url = urlparse(original_url)
+    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+    print(" trim_url_to_domain: ", base_url)
+    return {'base_url': base_url, 'original_url': original_url}
