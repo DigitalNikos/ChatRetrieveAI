d645c2affd8630e8449004e59a4d295f1f5fcd2d
Nikos Lithoxopoulos <nikoslithoxopoulos@Nikoss-Mac-mini.local>
2024-08-25 15:25:50 +0200

    Change UI

--- chatbot.py
+++ chatbot.py
@@ -4,31 +4,29 @@ import streamlit as st
 
 from rag.rag import ChatPDF
 from config import Config 
-from utils.extract_source import UploadStatus
+from utils.upload_source import UploadStatus, upload_document, upload_url
 from utils.text_doc_processing import trim_url_to_domain
 from utils.ui_helpers import add_divider, add_heading, add_padding, display_table, hide_file_names, add_centered_heading_with_description  
 
 
 st.set_page_config(page_title="ChatPDF", page_icon="🤖")
-cfg = Config()
-
 
 def display_messages():
     """
-    Display chat messages in the Streamlit app, ensuring consistent handling of message formats.
-    
-    This function checks if there are existing messages in the session state. If none exist, 
-    it initializes a default welcome message. It then iterates through the list of messages 
-    and displays each one according to its role (user, assistant, etc.).
-    """
-    print("\nCalling =>chatbot.py - display_messages()")
+    Display chat messages in the Streamlit app.
 
-    # Initialize default message if no messages exist
+    If no messages exist, initialize with a default assistant welcome message.
+    Iterate through session messages and display them according to their role 
+    (e.g., 'user' or 'assistant') in a chat format.
+    """
+    print("chatbot.py - display_messages()")
+
+    # Initialize default welcome message if no messages are present
     if not st.session_state["messages"]:
         domain_message = f"Welcome to our  {st.session_state['domain']} domain support chat! How can I assist you today?"
         st.session_state["messages"] = [{"role": "assistant", "content": domain_message}]
         
-    # Display each message in the chat
+    # Display all messages from the session state
     for msg in st.session_state["messages"]:
         role, content = (msg["role"], msg["content"]) if isinstance(msg, dict) else msg if isinstance(msg, tuple) else ("system", "Unexpected message format.") 
         st.chat_message(role).write(content)
@@ -39,17 +37,12 @@ def display_messages():
 
 def process_input():
     """
-    Process user input and get a response from the assistant.
+    Process user input and generate a response from the assistant.
 
-    This function checks the user input from the session state. If the input is valid (i.e., 
-    not empty after stripping whitespace), it sends the input to the assistant and retrieves 
-    a response. The user input and the assistant's response are then appended to the 
-    session state's message list, maintaining the conversation history.
-    
-    The function also manages the "thinking" spinner to provide visual feedback during the 
-    response generation process.
+    Validates and retrieves user input, sends it to the assistant for a response, 
+    and appends both the user input and assistant response to the chat history.
     """
-    print("\nCalling =>chatbot.py - process_input()")
+    print("chatbot.py - process_input()")
     
     # Check if the user input is valid
     if st.session_state["user_input"] and len(st.session_state["user_input"].strip()) > 0:
@@ -66,53 +59,34 @@ def process_input():
 
 def read_and_save_file(domain: str, source_type: str):
     """
-    Handle file or URL upload and initiate document ingestion.
-    
-    This function processes the upload of a file or URL based on the provided source type 
-    ('.pdf', '.docx', '.txt', or 'url'). It retrieves the appropriate action 
-    from the configuration's `UPLOAD_ACTIONS` dictionary and executes it, passing the 
-    domain and Streamlit context (`st`) as arguments.
-    
-    If the `source_type` is invalid or not recognized, the function raises a ValueError, 
-    providing feedback on the expected file types or URL.
-    
-    Raises:
-    - ValueError: If the `source_type` is not one of the expected values ('.pdf', '.docx', 
-      '.txt', or 'url').
-    """
-    print("\nCalling =>chatbot.py - read_and_save_file()")
+    Process file or URL uploads and initiate document ingestion.
 
-    # Retrieve the appropriate action based on the source type
-    action = cfg.UPLOAD_ACTIONS.get(source_type)
+    Depending on the `source_type` ('document' or 'url'), this function handles the 
+    upload, executes the relevant action (uploading a .pdf, .text, .docx or processing a URL), 
+    and updates the session state with the result.
+
+    Raises a ValueError if the `source_type` is invalid.
+    """
+    print("chatbot.py - read_and_save_file()")
     
-    # Raise an error if the source type is invalid
-    if not action:
-            raise ValueError(f"Invalid source type: {source_type}. Expected '.pdf', '.docx', '.txt' or 'url'.")
+    if source_type == "document":
+        status = upload_document(domain, st)  
+    elif source_type == "url":
+        status = upload_url(domain, st)  
+    else:
+        raise ValueError(f"Invalid source type: {source_type}. Expected 'document' or 'url'.")
     
-    # Execute the action if valid
-    status =action(domain, st)
-    print('Status Read_and_save_files:  ', status)
-    
-    # Handle URL status updates
     if source_type == "url":
-        st.session_state["error_status"] = status['status']
-    
-    # Handle document status updates
+        st.session_state["url_status"] = status['status']
     elif source_type == "document":
-        st.session_state['error_document'] = {'status': status['status'], 'file_name': status.get('file_name')}
+        st.session_state['document_status'] = {'status': status['status'], 'file_name': status.get('file_name')}
     
 
 def initialize_session_state():
     """
     Initialize the session state if it is currently empty.
-
-    This function checks if the `st.session_state` dictionary is empty. If it is,
-    it calls `clear_session_state` to set up the necessary initial values for the session.
-    
-    This ensures that the session state has the required keys and values before 
-    the application proceeds, preventing potential errors due to missing state data.
     """
-    print("\nCalling =>chatbot.py - initialize_session_state()")
+    print("chatbot.py - initialize_session_state()")
     
     if len(st.session_state) == 0:
         clear_session_state()
@@ -120,23 +94,25 @@ def initialize_session_state():
 
 def clear_session_state():
     """
-    Clear and initialize the session state with default values.
-
-    This function resets the `st.session_state` dictionary to ensure a clean state for the application.
-
-    This function is typically called when the session is first initialized or needs to be reset.
+        Reset the session state to default values.
     """
     st.session_state["messages"] = []
-    st.session_state["assistant"] = ChatPDF(cfg)
+    st.session_state["assistant"] = ChatPDF()
     st.session_state["domain"] = ""
     st.session_state['urls_store'] = []
     st.session_state['url_upload'] = "" 
     st.session_state['file_names'] = []
-    st.session_state["error_document"] = None
+    st.session_state["document_status"] = None
+    st.session_state["file_hashes"] = []
 
 
 @st.dialog("Show Uploaded Files")
 def show_file_names(names):
+    """
+    Display a list of uploaded file names in a table format.
+    
+    If no files are uploaded, a message is displayed.
+    """
     if not names:
         st.write("No files uploaded.")
         return
@@ -144,10 +120,19 @@ def show_file_names(names):
     headers = ["#", "File name"]
     rows = [[i + 1, name] for i, name in enumerate(names)]
     display_table(headers, rows, max_visible_items=15)
-
+    
 
 @st.dialog("Upload URL")
-def upload_url():
+def ingest_url():
+    """
+    Validate and upload a user-provided URL.
+
+    Ensures the URL format is valid, checks for connectivity, and attempts 
+    to upload the URL for document ingestion. Displays appropriate messages 
+    based on success or failure of the upload process.
+    """
+    print("chatbot.py - ingest_url()")
+    
     st.write(f"Add URL in the input box below and click 'Submit'.")
     url = st.text_input("Enter URL...")
     
@@ -165,7 +150,7 @@ def upload_url():
             st.rerun()
         
         try:
-            response = requests.head(url, headers=headers, timeout=5)  # Attempt a HEAD request to the URL
+            response = requests.head(url, headers=headers, timeout=5)  
             print("Status code:", response.status_code)
 
             if response.status_code != 200:
@@ -179,13 +164,13 @@ def upload_url():
         read_and_save_file(st.session_state["domain"], "url")
         
         # Check if the URL was successfully processed
-        if st.session_state.get("error_status") == UploadStatus.SUCCESS:
+        if st.session_state.get("url_status") == UploadStatus.SUCCESS:
             url_cut = trim_url_to_domain(url)
             st.session_state['urls_store'].append(url_cut)
             st.success("URL uploaded successfully.", icon="✅")
             time.sleep(4)
             st.rerun()
-        elif st.session_state.get("error_status") == UploadStatus.INVALID_DOMAIN:
+        elif st.session_state.get("url_status") == UploadStatus.INVALID_DOMAIN:
             st.warning("URL does not fall within the specified domain.", icon="⚠️")
         else:
             st.error("Failed to upload the URL.", icon="🚨")
@@ -193,6 +178,11 @@ def upload_url():
 
 @st.dialog("Show Uploaded URLs")
 def show_uploaded_urls(names):
+    """
+    Display a table of uploaded URLs.
+
+    If no URLs are uploaded, a message is shown.
+    """
     if not names:
         st.write("No files uploaded.")
         return
@@ -202,35 +192,23 @@ def show_uploaded_urls(names):
     display_table(headers, rows, max_visible_items=15)
 
 
+
 def page():
     """
-    Main function to handle the Streamlit page layout and interactions.
+    Main function to render the Streamlit app layout and manage interactions.
 
-    This function sets up the user interface and manages interactions within the Streamlit app. 
-    It initializes the session state, displays headers and instructions, and handles user input 
-    and file uploads. The function is structured to dynamically update the interface based on 
-    whether a domain is set or not.
-
-    Key functionalities include:
-    - Initializing session state and clearing it when necessary.
-    - Displaying appropriate headers and captions based on the current domain.
-    - Managing user input for setting a domain and submitting messages.
-    - Handling file uploads and URLs for document ingestion.
-    - Displaying chat messages and managing the interaction flow.
-
-    The function also includes error handling for invalid URLs and allows the user to reset the 
-    domain context, clearing the session state and restarting the interaction.
-
-    Raises:
-    - ValueError: If the file or URL upload encounters an unsupported format or type.
+    Handles session state initialization, domain input, file/URL uploads, 
+    and user interactions. Dynamically updates the interface based on whether 
+    a domain is set. Displays chat messages, manages file and URL ingestion, 
+    and allows users to reset the domain and session state.
     """
-
-    print("\nCalling =>chatbot.py - page()")
+    print("chatbot.py - page()")
+    
     initialize_session_state()
 
-    
     if st.session_state["domain"].strip() == "":
-        st.header("Chatbot 🤖 💬")
+        add_padding(padding_top=200)
+        st.header("Chatbot for specific Domain 🤖 💬")
         st.caption("ChatRetrieveAI: Using RAG for Document📚 and Web search Interaction🔎.")
         
         with st.form('chat_input_form'):
@@ -245,19 +223,6 @@ def page():
             st.form_submit_button('Add Domain',  use_container_width=True)     
             st.session_state["domain"] = domain
             st.session_state["assistant"].set_domain(domain)
-        
-        add_divider(padding_top=0)
-        add_centered_heading_with_description("Configure Retrieval Settings", "Customize the retrieval process by adjusting the parameters below.")
-        add_padding(padding_top=50)
-               
-        col1, col2, col3 = st.columns([1,4,1])
-        with col2:
-            model_temperature = st.slider("Model Temperature", min_value=0.0, max_value=1.0, step=0.1, value=0.0)
-            chunk_size = st.slider("Chunk Size", min_value=20, max_value=1000, step=20, value=512)
-            chunk_overlap = st.slider("Chunk Overlap", min_value=0, max_value=1000, step=5, value=51)
-            n_documents_to_retrieve = st.slider("Nr. of retrieve Documents", min_value=0, max_value=20, step=1, value=4)
-            retrieve_score_threshold = st.slider("Retrieve score threshold", min_value=0.0, max_value=1.0, step=0.1, value=0.7)
-            st.session_state["assistant"].cfg.update_splitter_params(model_temperature, chunk_size, chunk_overlap, n_documents_to_retrieve, retrieve_score_threshold)
             
         if st.session_state["domain"].strip() != "":
             st.rerun() 
@@ -289,22 +254,22 @@ def page():
             add_heading("📝 Upload Files", level=2, padding_bottom=0)         
             st.markdown(hide_file_names(), unsafe_allow_html=True)
 
-            file_upload = st.file_uploader(
+            file_uploader = st.file_uploader(
                 "Upload document",
                 type=["pdf", "docx", "txt"],
                 key="file_uploader",
                 on_change=read_and_save_file,
                 args = (st.session_state["domain"], "document" ),
                 label_visibility="collapsed",
-                accept_multiple_files=True,
+                accept_multiple_files=False,
             )
             
-            if st.session_state.get("error_document") and st.session_state["error_document"].get('status') == UploadStatus.INVALID_URL:
-                st.warning(f"The document '{st.session_state['error_document']['file_name']}' has already been uploaded.")
-            elif st.session_state.get("error_document") and st.session_state["error_document"].get('status') == UploadStatus.INVALID_DOMAIN:
-                st.error(f"The document '{st.session_state['error_document']['file_name']}' does not fall within the specified domain.")
-            elif st.session_state.get("error_document") and st.session_state["error_document"].get('status') == UploadStatus.SUCCESS:
-                st.success(f"Document {st.session_state['error_document']['file_name']} uploaded successfully.")   
+            if st.session_state.get("document_status") and st.session_state["document_status"].get('status') == UploadStatus.DUPLICATE_FILE:
+                st.warning(f"The document '{st.session_state['document_status']['file_name']}' has already been uploaded.")
+            elif st.session_state.get("document_status") and st.session_state["document_status"].get('status') == UploadStatus.INVALID_DOMAIN:
+                st.error(f"The document '{st.session_state['document_status']['file_name']}' does not fall within the specified domain.")
+            elif st.session_state.get("document_status") and st.session_state["document_status"].get('status') == UploadStatus.SUCCESS:
+                st.success(f"Document {st.session_state['document_status']['file_name']} uploaded successfully.")   
                 
             add_divider(padding_top=0)    
             
@@ -313,7 +278,7 @@ def page():
                 add_heading("🔗 Upload URL", level=2, padding_bottom=30)
                 
                 if st.button("Upload URL", use_container_width=True):
-                    upload_url()
+                    ingest_url()
                     
             except Exception as e:
                 st.error(f"An unexpected error occurred: {str(e)}")
@@ -326,5 +291,5 @@ def page():
 
 
 if __name__ == "__main__":
-    print("Calling =>chatbot.py - __main__")
+    print("chatbot.py - __main__")
     page()
