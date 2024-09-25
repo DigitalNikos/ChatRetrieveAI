118b6d267f25d339bd42fa7cbc7b1b3be3f412eb
Nikos Lithoxopoulos <nikoslithoxopoulos@Nikoss-Mac-mini.local>
2024-09-25 22:14:59 +0200

    add again TOKENIZERS_PARALLEL

--- tests/path_test/test_5_path.py
+++ tests/path_test/test_5_path.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 import unittest
 from rag.rag import ChatPDF
